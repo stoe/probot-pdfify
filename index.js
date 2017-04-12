8f6e62f2518e48251e08262460a4d7c77f0fcf31
Stefan Stölzle <stoe@github.com>
2017-04-12 16:47:42 +0200

    Stub out runtime

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-module.exports = (robot, execa = require('execa')) => {
-  const pdfify = async () => {
-    execa('node_modules/.bin/pdfify', []).then(res => {
-      console.log(res);
-    }).catch(err => {
-      console.log(err);
-    });
-  };
-
-  robot.on('push', pdfify);
+module.exports = async robot => {
+  robot.on('push', () => {});
+  robot.on('pull_request', () => {});
 };
